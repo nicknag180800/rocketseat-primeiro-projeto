f1a1342f23abef50ab7a09ebce25d84c1ca1acf0
nicknag180800 <grandnicolas@hotmail.com>
2021-12-21 10:02:24 -0300

    Initial commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList() {
+    return(
+        <section className="repository-list">
+            <h1>Lista de Repositorios</h1>
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p1>Forms in ReactJs</p1>
+                </li>
+            </ul>
+        </section>
+    );
+
+}
